1aa41d5df271e5b5caf1d9287653e4f5891d2978
Sarbesh Adhikari <sstudio985@gmail.com>
2026-07-20 23:20:57 +0545

    starting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.common',
+    'apps.rooms',
+    'apps.leaderboard',
+    'apps.gameplay',
+    'apps.quizzes',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
